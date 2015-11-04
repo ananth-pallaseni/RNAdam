1ae3813c2bf2eee93763a64994d5485d1bc0f626
ananth-pallaseni <ananth.pallaseni@gmail.com>
2015-11-04 15:00:31 -0800

    updated tests 15

--- rice-core/src/test/scala/org/bdgenomics/rice/algorithms/QuantifySuite.scala
+++ rice-core/src/test/scala/org/bdgenomics/rice/algorithms/QuantifySuite.scala
@@ -181,7 +181,7 @@ class QuantifySuite extends riceFunSuite {
               (true, "")
             }
             else {
-              val transcriptsMsg = "Count for transcript=" + t.toString + " for kmer=" + e.toString + " was " recCount + ". It should be " + expCount.toString
+              val transcriptsMsg = "Count for transcript=" + t.toString + " for kmer=" + e.toString + " was " recCount.toString + ". It should be " + expCount.toString
               (false, transcriptsMsg)
             }
           }
@@ -199,7 +199,7 @@ class QuantifySuite extends riceFunSuite {
       })
     val (transcriptsCountsCorrect, transcriptsMsg) = transcriptsCheck.map(t => (t._1, t._2 + "\n")).reduce((t1, t2) => (t1._1 && t2._1, t1._2 + t2._2))
 
-    val correct = kmersCorrect && transcriptCountsCorrect
+    val correct = kmersCorrect && transcriptsCountsCorrect
     val msg = kmerMsg + "\n" + transcriptsMsg
     if (correct) {
       true
